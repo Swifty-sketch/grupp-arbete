007e0777fddc94b9db4fa85b94dee7c54dbcf512
Swifty-sketch <marsel.davletov@chasacademy.se>
2024-05-28 14:20:08 +0200

    changed from tsx to jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import {
+  PaperAirplaneIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/outline";
+
+function App() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div>hi</div>
+  );
+}
+
+export default App;
